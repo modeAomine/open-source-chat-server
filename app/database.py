c23977d9ba0a71a5109e325b3247759214db323b
admin_github <admin@admin.admin>
2024-10-03 01:06:37 +0300

    1. current_user returns an SQLAlchemy  user object 2. fix user_services.update_user_field

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, registry
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://stamp:366520@localhost/forum"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/services/user_services.py
+++ app/services/user_services.py
@@ -6,7 +6,7 @@ from app.models.user import User
 from app.schemas import UserProfile
 
 
-def get_user_by_token(token: str, db: Session) -> UserProfile:
+def get_user_by_token(token: str, db: Session, return_pedic=False) -> UserProfile:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         username: str = payload.get("sub")
@@ -17,22 +17,30 @@ def get_user_by_token(token: str, db: Session) -> UserProfile:
         if not user:
             return None
 
-        return UserProfile(
-            username=user.username,
-            password=user.password,
-            name=user.name,
-            email=user.email,
-            filename=user.filename
-        )
+        if return_pedic:
+            return UserProfile(
+                username=user.username,
+                password=user.password,
+                name=user.name,
+                email=user.email,
+                filename=user.filename
+            )
+
+        return user
     except JWTError:
         return None
 
 
-def update_user_field(db: Session, username: str, field: str, value: str):
-    user = db.query(User).filter(User.username == username).first()
-    if not user:
-        return None
 
-    setattr(user, field, value)
+def update_user_field(db: Session, current_user: User, field: str, value: str):
+    # user = db.query(User).filter(User.username == username).first()
+
+    if field == 'password':
+        pass # Вызываем функцию изменяющую парольчик
+        print('ПОМЕНЯЛ ПАРОЛЬЧИК ТИПА')
+    else:
+        setattr(current_user, field, value)
+
     db.commit()
-    return user
+    return current_user
+
